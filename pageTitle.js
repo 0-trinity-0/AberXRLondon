8cc03956895177e3d6911aabbe48f7b65b5b4d41
0-trinity-0 <128228615+0-trinity-0@users.noreply.github.com>
2023-03-18 12:06:09 +0000

    inital commit

---
+++ pageTitle.js
@@ -0,0 +1,7 @@
+function shrinkTitle()
+{
+    setTimeout(()=>{
+        document.getElementById("title").style="height: 10vh; font-size: 4rem;";
+        //document.body.style="margin: 10px;";
+    }, 2000)
+}
