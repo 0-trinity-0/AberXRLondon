718a068559c154f09ae53d3b969e65d3968676e6
0-trinity-0 <128228615+0-trinity-0@users.noreply.github.com>
2023-03-21 16:44:57 +0000

    adding accessibility changes

--- pageTitle.js
+++ pageTitle.js
@@ -1,7 +1,28 @@
+var accessibilityColours = false;
+
 function shrinkTitle()
 {
     setTimeout(()=>{
         document.getElementById("title").style="height: 10vh; font-size: 4rem;";
-        //document.body.style="margin: 10px;";
+        document.getElementById("accessButton").style="opacity: 100%";
     }, 2000)
+}
+
+
+function toggleColors()
+{
+    if (accessibilityColours)
+    {
+    document.body.style = "var(--back)"
+    document.getElementById("accessButton").style="opacity: 100%; color: var(--safe)";
+    document.getElementById("accessButton").innerHTML = "Switch To Calmer Colours"
+    accessibilityColours = false
+    }
+    else
+    {
+    document.body.style = "background-color: #6f6f6f"
+    document.getElementById("accessButton").style="opacity: 100%; color: var(--back)";
+    document.getElementById("accessButton").innerHTML = "Switch To Brighter Colours"
+    accessibilityColours = true
+    }
 }
